a5e6b455dc0062afda85d317d519ab517b577885
SeungHyeon Hong <whi7ehyun@gmail.com>
2021-08-20 01:13:38 +0900

    CollectionViewCell을 viewcontroller 파일에 병합

--- INU_Notification/Source/ViewControllers/MajorSelectViewController.swift
+++ INU_Notification/Source/ViewControllers/MajorSelectViewController.swift
@@ -9,13 +9,17 @@ import UIKit
 
 class MajorSelectViewController: UIViewController {
     
+    @IBOutlet weak var selectCollectionView: UICollectionView!
     @IBOutlet weak var completeButton: UIButton!
     
     var majors: [Major] = []
     var majorDictionary: [String: [String]] = [:]
+    let cellID = "MajorSelectCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        selectCollectionView.dataSource = self
         
         let jsonDecoder: JSONDecoder = JSONDecoder()
         guard let dataAsset: NSDataAsset = NSDataAsset(name: "INU") else {
@@ -31,5 +35,33 @@ class MajorSelectViewController: UIViewController {
             print(error.localizedDescription)
         }
         
+    }
+}
+
+class MajorSelectCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var collegeButton: UIButton!
+    
+    /**
+    Cell에 UIButton을 추가 및 레이아웃을 설정해주는 역할을 합니다.
+     */
+//    private func setupView() {
+//        contentView.addSubview(collegeButton)
+//        collegeButton.snp.makeConstraints { maker in
+//            maker.edges.equalToSuperview().inset(10)
+//        }
+//    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = frame.height / 2
+        layer.borderWidth = 2
+        layer.borderColor = UIColor(hex: "14286F").cgColor
+    }
+
+    func configure(name: String?) {
+        guard let name = name else { return }
+        collegeButton.setTitle("\(name)", for: .normal)
+        collegeButton.setTitleColor(.black, for: .normal)
     }
 }
