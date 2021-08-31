ff70e6da971b1a8ff5711732070ca3f959accf1a
SeungHyeon Hong <whi7ehyun@gmail.com>
2021-08-31 20:34:51 +0900

    home viewcontroller 생성

---
+++ INU_Notification/Source/ViewControllers/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  INU_Notification
+//
+//  Created by 홍승현 on 2021/08/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
