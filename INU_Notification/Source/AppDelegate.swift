75a40133c283302d7733a510f4cd4e684f7a69b6
SeungHyeon Hong <whi7ehyun@gmail.com>
2021-09-14 15:29:10 +0900

    불필요한 코드 삭제

--- INU_Notification/Source/AppDelegate.swift
+++ INU_Notification/Source/AppDelegate.swift
@@ -21,16 +21,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                 return
             }
             print("Success in APNS registry")
-            
         }
         application.registerForRemoteNotifications()
         return true
     }
     
     // MARK: Messaging
-
+    
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        //        print("Firebase registration token: \(String(describing: fcmToken))")
         
         let dataDict: [String: String] = ["token": fcmToken ?? ""]
         NotificationCenter.default.post(
@@ -43,11 +41,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                 print("Error fetching FCM registration token: \(error)")
             } else if let token = token {
                 print("Remote FCM registration token: \(token)")
-                UserDefaults.standard.set(token, forKey: "FCMToken")
+                UserDefaults.standard.set(token, forKey: "FCMToken") // Token 저장
             }
         }
-        // TODO: If necessary send token to application server.
-        // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
     
     func application(_ application: UIApplication,
@@ -55,21 +51,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         Messaging.messaging().apnsToken = deviceToken
     }
     
-    // Receive displayed notifications for iOS 10 devices.
     // 앱이 foreground에 있을 때 푸시 알림이 오면 이 메서드가 호출된다.
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
                                     -> Void) {
         let userInfo = notification.request.content.userInfo
-
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-
-        // Print full message.
-        print("FOREGROUNDMESSAGE")
         print(userInfo)
-
+        
         // Change this to your preferred presentation option
         completionHandler([[.banner, .list, .sound]])
     }
@@ -79,35 +68,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                                 didReceive response: UNNotificationResponse,
                                 withCompletionHandler completionHandler: @escaping () -> Void) {
         let userInfo = response.notification.request.content.userInfo
-        // ...
-
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-
         print("NOTIFICATION PUSH CLICKED")
         // Print full message.
         print(userInfo)
-        print(userInfo["body"]) // Optional(테스트링크)
-        print(userInfo["keyword"]) // Optional(장학)
-        print(userInfo["title"]) // Optional(테스트장학)
-
+        print(userInfo["body"]!) // Optional(테스트링크)
+        print(userInfo["keyword"]!) // Optional(장학)
+        print(userInfo["title"]!) // Optional(테스트장학)
+        
         completionHandler()
-    }
-    
-    func application(_ application: UIApplication,
-                     didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult)
-                        -> Void) {
-        // If you are receiving a notification message while your app is in the background,
-        // this callback will not be fired till the user taps on the notification launching the application.
-        // TODO: Handle data of notification
-
-        // With swizzling disabled you must let Messaging know about the message, for Analytics
-        // Messaging.messaging().appDidReceiveMessage(userInfo)
-        // Print full message.
-        print("WHAT IS THIS?")
-         print(userInfo)
-
-        completionHandler(UIBackgroundFetchResult.newData)
     }
 }
