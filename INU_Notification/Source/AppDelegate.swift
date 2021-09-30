0d400cc535082dd0fa1c650344dd1a6dad9e4e9e
SeungHyeon Hong <whi7ehyun@gmail.com>
2021-09-30 10:28:18 +0900

    푸시알림으로 데이터 가져오는 방식 제거

--- INU_Notification/Source/AppDelegate.swift
+++ INU_Notification/Source/AppDelegate.swift
@@ -50,9 +50,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
                      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
     }
-    
-    // 앱이 Background에 있을 때 알림이 오면 콜백되는 함수가 필요한데.. 없어보인다.
-    
+        
     // 앱이 foreground에 있을 때 푸시 알림이 오면 이 메서드가 호출된다.
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 willPresent notification: UNNotification,
@@ -61,13 +59,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let userInfo = notification.request.content.userInfo
         
         Messaging.messaging().appDidReceiveMessage(userInfo)
-        
-        print("NOTIFICATION FOREGROUND")
-        print(userInfo["link"] ?? "Link does not exist.") // Optional(테스트링크), 공지 링크
-        print(userInfo["keyword"] ?? "Keyword does not exist.") // Optional(장학), 키워드
-        print(userInfo["name"] ?? "title does not exist.") // Optional(테스트장학), 제목
-        noticeProcessing(userInfo)
-        
+                
         // Change this to your preferred presentation option
         completionHandler([[.banner, .list, .sound]])
     }
@@ -80,37 +72,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         
         Messaging.messaging().appDidReceiveMessage(userInfo)
         
-        print("NOTIFICATION PUSH CLICKED")
-        print(userInfo["link"] ?? "Link does not exist.") // Optional(테스트링크), 공지 링크
-        print(userInfo["keyword"] ?? "Keyword does not exist.") // Optional(장학), 키워드
-        print(userInfo["name"] ?? "title does not exist.") // Optional(테스트장학), 제목
-        noticeProcessing(userInfo)
         completionHandler()
-    }
-    
-    private func noticeProcessing(_ userInfo: [AnyHashable : Any]) {
-        guard let url: String = userInfo["link"] as? String,
-              let keyword: String = userInfo["keyword"] as? String,
-              let title: String = userInfo["name"] as? String else {
-                  return
-              }
-        var noticeArray: [Notice] {
-            get {
-                var keywords: [Notice]?
-                if let data = UserDefaults.standard.data(forKey: keyword) {
-                    keywords = try? PropertyListDecoder().decode([Notice].self, from: data)
-                }
-                return keywords ?? []
-            }
-            set {
-                UserDefaults.standard.set(try? PropertyListEncoder().encode(newValue), forKey: keyword)
-            }
-        }
-        let now: Date = Date()
-        let date: DateFormatter = DateFormatter()
-        date.dateFormat = "yyyy.MM.dd"
-        let time = date.string(from: now)
-        let notice: Notice = Notice(title: title, time: time, url: url)
-        noticeArray.append(notice)
     }
 }
